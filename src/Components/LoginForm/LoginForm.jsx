b18c4fd8b037935fe9091f86259812e908f82f27
nicope96 <ngpa096@gmail.com>
2024-06-05 22:48:33 -0500

    login

---
+++ src/Components/LoginForm/LoginForm.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import './LoginForm.css';
+import { FaUserMd, FaUnlock, FaEnvelope, FaEye } from "react-icons/fa";
+
+const LoginForm = () => {
+
+    const [action, setAction] = useState('');
+
+    const registerLink = () => {
+        setAction(' active');
+    };
+    const LoginLink = () => {
+        setAction('');
+    };
+
+    return (
+        <div className={`wrapper${action}`}>
+            <div className="form-box login">
+                <form action="">
+                    <h1>Login</h1>
+                    <div className="input-box">
+                        <input type="text" id="email" placeholder="EMAIL" />
+                        <FaUserMd className="icon" />
+                    </div>
+                    <div className="input-box">
+                        <input type="password" id="contrasena" placeholder="CONTRASEÑA" />
+                        <FaUnlock className="icon" />
+                    </div>
+
+                    <div className="remember-forgot">
+                        <label><input type="checkbox" />Recordar Usuario</label>
+                        <a href="#">Olvidó su contraseña?</a>
+                    </div>
+
+                    <button type="button">INGRESAR</button>
+                    <div className="register-link">
+                        <p>No cuentas con un usuario? <a href="#" onClick={registerLink}>Registrar</a></p>
+                    </div>
+                </form>
+            </div>
+
+            <div className="form-box register">
+                <form action="">
+                    <h1>REGISTRAR</h1>
+
+                    <div className="input-box">
+                        <input type="text" id="email" placeholder="EMAIL" />
+                        <FaEnvelope className="icon" />
+                    </div>
+                    <div className="input-box">
+                        <input type="text" id="nombre" placeholder="NOMBRE" />
+                        <FaUserMd className="icon" />
+                    </div>
+                    <div className="input-box">
+                        <input type="text" id="documento" placeholder="DOCUMENTO" />
+                        <FaEye className="icon" />
+                    </div>
+                    <div className="input-box">
+                        <input type="password" id="contrasena" placeholder="CONTRASEÑA" />
+                        <FaUnlock className="icon" />
+                    </div>
+                    <div className="input-box">
+                        <input type="password" id="contrasenaCon" placeholder="CONFIRMAR CONTRASEÑA" />
+                        <FaUnlock className="icon" />
+                    </div>
+                   
+                    <button type="button">REGISTRAR</button>
+                   
+                    <div className="remember-forgot">
+                        <label><input type="checkbox" />Aceptar Términos y Condiciones</label>
+                    </div>
+
+                    <div className="register-link">
+                        
+                        <p>Ya estás registrado? <a href="#" onClick={LoginLink}>INGRESAR</a></p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default LoginForm;
